57abff34d39dc8ba191b8d963eeccb830161b9a9
Joan Domingo <joands@gmail.com>
2021-07-08 13:46:11 +0200

    comment out import and code affected

--- read_bird_list.py
+++ read_bird_list.py
@@ -1,19 +1,19 @@
-from google_images import main
+#from google_images import main
 
 bird_list_file = open('llista_ocells_de_catalunya.txt', 'r')
 species = bird_list_file.readlines()
  
 
-def download_specie_images():
-    for i, line in enumerate(species, start=1):
+#def download_specie_images():
+#    for i, line in enumerate(species, start=1):
 
-        normalized_line = line.strip()
-        scientific_name = normalized_line.split(" (")[1].split(")")[0]
-        catalan_name = normalized_line.split(" (")[0]
+#        normalized_line = line.strip()
+#        scientific_name = normalized_line.split(" (")[1].split(")")[0]
+#        catalan_name = normalized_line.split(" (")[0]
 
-        print("Specie {}: {} ({})".format(i, catalan_name, scientific_name))
+#        print("Specie {}: {} ({})".format(i, catalan_name, scientific_name))
 
-        main(scientific_name, catalan_name, 100)
+#        main(scientific_name, catalan_name, 100)
 
 
 def read_scientific_name(name):
